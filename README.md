550f7d65e9e245b191e6f48c923347ba397d895d
Zonggui Chen <ggchenzonggui@qq.com>
2022-04-01 09:59:50 +0800

    Add PigzFile and test.

---
+++ README.md
@@ -0,0 +1,59 @@
+# Pigz wrapper
+
+This repository is a wrapper for python to utilize pigz to compress or decompress data in parallel.
+
+At first, you should add the path of this repository to your PYTHONPATH, or copy the pigz.py to your site-packages directory.
+
+For reading:
+
+    from pigz import PigzFile
+    with PigzFile("input.gz") as f:
+        for line in f:
+            print(line)
+
+For writing:
+
+    from pigz import PigzFile
+    threads = 4 # default
+    with PigzFile("output.gz", "wt", threads) as fw:
+        for line in lines:
+            fw.write(line)
+
+For testing:
+
+    ./test_pigz.py
+
+the output tis as follow:
+
+    ============================================================
+    Read by gzip package:
+    Number: 1, Time: 889.35 ms
+    Number: 2, Time: 931.01 ms
+    Number: 3, Time: 967.92 ms
+    Number: 4, Time: 920.07 ms
+    Number: 5, Time: 958.66 ms
+    Average time: 933.40 ms
+    ============================================================
+    Read by pigz subprocess:
+    Number: 1, Time: 483.14 ms
+    Number: 2, Time: 414.45 ms
+    Number: 3, Time: 413.28 ms
+    Number: 4, Time: 460.89 ms
+    Number: 5, Time: 468.44 ms
+    Average time: 448.04 ms
+    ============================================================
+    Write by gzip package:
+    Number: 1, Time: 66915.84 ms
+    Number: 2, Time: 67248.85 ms
+    Number: 3, Time: 65625.26 ms
+    Number: 4, Time: 65328.97 ms
+    Number: 5, Time: 65264.58 ms
+    Average time: 66076.70 ms
+    ============================================================
+    Write by pigz subprocess:
+    Number: 1, Time: 3522.61 ms
+    Number: 2, Time: 3740.81 ms
+    Number: 3, Time: 3542.37 ms
+    Number: 4, Time: 3541.36 ms
+    Number: 5, Time: 4064.07 ms
+    Average time: 3682.24 ms
---
+++ test_pigz.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+import os
+import time
+import gzip
+from pigz import PigzFile
+
+
+def read_by_gzip(path):
+    with gzip.open(path, "rt") as f:
+        lines = [line for line in f]
+    return lines
+        
+def read_by_pigz(path):
+    with PigzFile(path, "rt") as f:
+        lines = [line for line in f]
+    return lines
+
+def write_by_gzip(path, lines):
+    with gzip.open(path, "wt") as fw:
+        for line in lines:
+            fw.write(line)
+            
+def write_by_pigz(path, lines):
+    with PigzFile(path, "wt") as fw:
+        for line in lines:
+            fw.write(line)
+        
+def main():
+    PATH1 = "GRCh39.p13.chr1.1000000.fa.gz"
+    PATH2 = "out.test.gz"
+    N = 5
+
+    print("=" * 60)
+    print("Read by gzip package:")
+    ts = []
+    for i in range(N):
+        t1 = time.time()
+        lines = read_by_gzip(PATH1)
+        t2 = time.time()
+        t3 = (t2 - t1) * 1000
+        ts.append(t3)
+        print("Number: %d, Time: %.2f ms" % (i + 1, t3))
+    print("Average time: %.2f ms" % (sum(ts) / N))
+    
+    print("=" * 60)
+    print("Read by pigz subprocess:")
+    ts = []
+    for i in range(N):
+        t1 = time.time()
+        lines = read_by_pigz(PATH1)
+        t2 = time.time()
+        t3 = (t2 - t1) * 1000
+        ts.append(t3)
+        print("Number: %d, Time: %.2f ms" % (i + 1, t3))
+    print("Average time: %.2f ms" % (sum(ts) / N))
+        
+    print("=" * 60)
+    print("Write by gzip package:")
+    ts = []
+    for i in range(N):
+        t1 = time.time()
+        write_by_gzip(PATH2, lines)
+        t2 = time.time()
+        t3 = (t2 - t1) * 1000
+        ts.append(t3)
+        print("Number: %d, Time: %.2f ms" % (i + 1, t3))
+    print("Average time: %.2f ms" % (sum(ts) / N))
+
+    print("=" * 60)
+    print("Write by pigz subprocess:")
+    ts = []
+    for i in range(N):
+        t1 = time.time()
+        write_by_pigz(PATH2, lines)
+        t2 = time.time()
+        t3 = (t2 - t1) * 1000
+        ts.append(t3)
+        print("Number: %d, Time: %.2f ms" % (i + 1, t3))
+    print("Average time: %.2f ms" % (sum(ts) / N))
+    
+    if os.path.exists(PATH2):
+        os.remove(PATH2)
+
+
+if __name__ == '__main__':
+    main()
+    
